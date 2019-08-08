94ea977f5c1d0d11d9dfe413da6e910b06a344ff
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-08 12:20:04 -0500

    Add responsiveness

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import '../styles/Title.css';
 
 const Title = ({ title }) => {
   return (
-    <Container>
+    <Container id="title">
       <Row>
-        <Col>
+        <Col lg="auto" className="text-center">
           <h1>{title.title}</h1>
         </Col>
+        <Col />
       </Row>
       <Row>
-        <Col md={3}>
+        <Col sm={10} md={4} lg={3} className="text-center">
           <img src={title.image} alt="title" />
         </Col>
-        <Col>
+        <Col sm={11} md={8} lg={9} className="justify-content-center">
           <Row>
             Synopsis:
             {' '}
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import '../styles/Jumbotron.css';
 
 const Welcome = () => (
-  <Jumbotron>
-    <h1>Quick Flix</h1>
-    <p>Find a decent movie fast!</p>
+  <Jumbotron fluid>
+    <Container className="text-center">
+      <div id="logo"><b>Quick Flix</b></div>
+      <p id="subtitle"><i>Find a decent movie decently fast!</i></p>
+    </Container>
   </Jumbotron>
 );
 
